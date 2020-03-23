095767af765e910595dbdf3e3eb7ff76b6de7b99
Tyler Haas <tyler.haas@octanner.com>
2020-03-23 10:32:45 -0600

    add reproducible example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from "@material-ui/core/TextField";
+import "./App.css";
+
+const Typeahead = () => {
+  const unfilteredOptions = [
+    { firstName: "Joe", lastName: "Bob" },
+    { firstName: "Jill", lastName: "Jackson" },
+    { firstName: "Frank", lastName: "Sinatra" },
+    { firstName: "Billy", lastName: "Jean" },
+    { firstName: "Meryl", lastName: "Streep" },
+    { firstName: "Bob", lastName: "Saget" },
+    { firstName: "Zoe", lastName: "Deschanel" },
+    { firstName: "Will", lastName: "Ferrell" },
+    { firstName: "John", lastName: "Candy" }
+  ];
+  return (
+    <Autocomplete
+      id="free-solo-demo"
+      freeSolo
+      options={unfilteredOptions.map(
+        option => `${option.firstName} ${option.lastName}`
+      )}
+      renderInput={params => (
+        <TextField
+          {...params}
+          label="freeSolo"
+          margin="normal"
+          variant="outlined"
+        />
+      )}
+    />
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Typeahead />
     </div>
   );
 }
